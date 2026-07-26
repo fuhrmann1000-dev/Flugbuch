e15bf9ca59bb071ff76534ce342c288e0e33f82e
Reiser2311 <cuestagarciaivan@gmail.com>
2026-07-26 21:16:18 +0200

    Id added for flights

--- backend/src/main/java/de/windenshelter/flugbuch/dto/FlugbuchEintragDto.java
+++ backend/src/main/java/de/windenshelter/flugbuch/dto/FlugbuchEintragDto.java
@@ -1,10 +1,17 @@
 package de.windenshelter.flugbuch.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import java.time.LocalDateTime;
 
 @Data
 public class FlugbuchEintragDto {
+    private Long id;
+
+    @JsonFormat(pattern = "dd.MM.yyyy HH:mm")
+    @Schema(type = "string", pattern = "dd.MM.yyyy HH:mm", example = "26.07.2026 19:05")
     private LocalDateTime flugDatum;
     private String kundenNummer;
     private String nameDesPiloten;
